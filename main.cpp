ad4f5cd5e6de450358edf35cbe790612eaa32ede
cmhtrumpeter <162759023+cmhtrumpeter@users.noreply.github.com>
2025-04-22 11:00:54 -0400

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
-// MazeSolver.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <SFML/Graphics.hpp>
-#include <iostream>
-#include <vector>
-#include <string>
 #include "MazeRenderer.h"
-#include "MazeGenerator.h" 
+#include "MazeGenerator.h"
 
 int main() {
     // Step 1: Generate a new maze with at least 100,000 elements
+    int rows = 401;  // Must be odd to work with the maze generation algorithm
+    int cols = 251;  // Same here
+    int tileSize = 5;
+    int windowWidth = 1800;
+    int windowHeight = 800;
 
-    // Step 2: Convert generated maze to vector<string>
-    
+    MazeGenerator generator(rows, cols);
+    generator.generate();
+
     // Step 3: Run the renderer
-    
+    MazeRenderer renderer(generator, tileSize, windowWidth, windowHeight);
+    renderer.run();
+
     return 0;
 }
